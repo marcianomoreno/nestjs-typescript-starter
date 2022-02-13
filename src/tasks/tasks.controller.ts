b3feb9c15efdd1092da3816c2eb26ae44399a042
Marciano Moreno <marciano_moreno@outlook.com>
2022-02-12 20:49:58 -0600

    Implemeting baseline getTasksById in controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
     @Get()
     getTasks():any {
         return [ {id:0} ];
+    }
+
+    @Get(':id')
+    getTaskById(@Param('id') id: string): any {
+        return {
+            id
+        }
     }
 }
