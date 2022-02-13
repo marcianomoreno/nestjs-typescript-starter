f3fc007a81f7d792024bdc3498898852c70db784
Marciano Moreno <marciano_moreno@outlook.com>
2022-02-13 08:06:11 -0600

    Added ApiTags and CreatedResponse for createTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,10 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { Task } from './entities/task.entity';
 
+@ApiTags('tasks')
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
@@ -15,6 +18,7 @@ export class TasksController {
         return this.tasksService.findById(Number(id));
     }
 
+    @ApiCreatedResponse({type: Task})
     @Post()
     createTask(@Body() body: CreateTaskDto): any {
         return this.tasksService.createTask(body);
