f6287c7d9ac30114087db8a5d8a55a16939afdb8
Marciano Moreno <marciano_moreno@outlook.com>
2022-02-13 08:10:56 -0600

    Added ApiOkResponse for getTasks and getTasksById

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,18 +1,21 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { Task } from './entities/task.entity';
 
 @ApiTags('tasks')
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
+
+    @ApiOkResponse({type: Task, isArray: true})
     @Get()
     getTasks():any {
         return this.tasksService.findAll();
     }
 
+    @ApiOkResponse({type: Task, isArray: false})
     @Get(':id')
     getTaskById(@Param('id') id: string): any {
         return this.tasksService.findById(Number(id));
