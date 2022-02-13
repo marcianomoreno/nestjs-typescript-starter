5f71b50bb05a50e0351d869060c2fb0f4aee0bd3
Marciano Moreno <marciano_moreno@outlook.com>
2022-02-13 09:07:17 -0600

    Added contacts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersService } from './users/users.service';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, ContactsModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
