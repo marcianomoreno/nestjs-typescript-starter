00483cae6259581d25e5b8316dfef335b19a2b17
Marciano Moreno <marciano_moreno@outlook.com>
2022-02-12 21:00:23 -0600

    Implemented findAll, findById in service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: any = [ {id:0, name: 'Marciano'} ];
+
+    findAll() {
+        return this.tasks;
+    }
+
+    findById(taskId: number) {
+        //TODO: Auto parse ID
+        return this.tasks.find(task => task.id === taskId);
+    }
+}
