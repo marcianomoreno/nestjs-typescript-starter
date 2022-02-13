4881918a87f5b90852d6a02e54c8f77581d6e2ce
Marciano Moreno <marciano_moreno@outlook.com>
2022-02-12 21:00:58 -0600

    Called findAll and findById from service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+    constructor(private tasksService: TasksService) {}
     @Get()
     getTasks():any {
-        return [ {id:0} ];
+        return this.tasksService.findAll();
     }
 
     @Get(':id')
     getTaskById(@Param('id') id: string): any {
-        return {
-            id: Number(id)
-        }
+        return this.tasksService.findById(Number(id));
     }
 }
