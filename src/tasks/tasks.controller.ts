99e5c4908aa7bc387d96255f251dd656f06901bd
Marciano Moreno <marciano_moreno@outlook.com>
2022-05-02 12:36:33 -0500

    Fixed linint issues

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,36 +1,56 @@
-import { Body, Controller, Get, NotFoundException, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Task } from './entities/task.entity';
 
 @ApiTags('tasks')
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {}
 
-    @ApiOkResponse({type: Task, isArray: true})
-    @ApiQuery({name: 'name', required: false})
-    @Get()
-    getTasks(@Query('name') name: string):any {
-        return this.tasksService.findAll(name);
+  @ApiOkResponse({ type: Task, isArray: true })
+  @ApiQuery({ name: 'name', required: false })
+  @Get()
+  getTasks(@Query('name') name: string): any {
+    return this.tasksService.findAll(name);
+  }
+
+  @ApiOkResponse({
+    type: Task,
+    isArray: false,
+    description: 'Returns a task by id',
+  })
+  @ApiNotFoundResponse()
+  @Get(':id')
+  getTaskById(@Param('id', ParseIntPipe) id: number): any {
+    const user = this.tasksService.findById(id);
+    if (!user) {
+      throw new NotFoundException();
     }
+    return user;
+  }
 
-    @ApiOkResponse({type: Task, isArray: false, description: 'Returns a task by id'})
-    @ApiNotFoundResponse()
-    @Get(':id')
-    getTaskById(@Param('id', ParseIntPipe) id: number): any {
-        const user = this.tasksService.findById(id);
-        if (!user) {
-            throw new NotFoundException();
-        }
-        return user;
-    }
-
-    @ApiCreatedResponse({type: Task})
-    @ApiBadRequestResponse()
-    @Post()
-    createTask(@Body() body: CreateTaskDto): any {
-        return this.tasksService.createTask(body);
-    } 
+  @ApiCreatedResponse({ type: Task })
+  @ApiBadRequestResponse()
+  @Post()
+  createTask(@Body() body: CreateTaskDto): any {
+    return this.tasksService.createTask(body);
+  }
 }
